5634eb3c56576f4cfeeefb4b6f005117dc83f996
William Pinto <williamp0403@gmail.com>
2024-09-14 08:03:47 -0400

    Cambios en app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express, { json } from 'express'
 import { createMovieRouter } from './routes/movies.js'
 import { corsMiddleware } from './middlewares/cors.js'
+import { MovieModel } from './models/mysql/movie.js'
 
-export const createApp = ({ movieModel }) => {
+
     const app = express()
     const PORT = process.env.PORT ?? 3000
     
@@ -10,11 +11,10 @@ export const createApp = ({ movieModel }) => {
     app.use(corsMiddleware())
     app.disable('x-powered-by')
     
-    app.use('/movies', createMovieRouter({ movieModel }))
+    app.use('/movies', createMovieRouter({ movieModel: MovieModel }))
     
     app.listen(PORT, ()=> {
         console.log(`server listening on port http://localhost:${PORT}`)
     })
-}
 
 
